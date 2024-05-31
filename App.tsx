7d7fa7313c1c62716fb5e145ba78af9fa8148e5d
Kashyap Patel <kashyap.patel@gtcsys.com>
2024-05-31 12:07:31 +0530

    background changed

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <View
       style={{
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'red',
         justifyContent: 'center',
         alignItems: 'center',
       }}>
